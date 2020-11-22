4248dec1766801dea370467c9a366e3412f51725
Bharat Ranjan <ranjanbharat54@gmail.com>
2020-11-22 21:13:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.delete('/:id', async (req, res) => {
 	res.redirect('/');
  });
 
-app.listen(3000, () => {
+app.listen( process.env.PORT || 3000, process.env.IP, () => {
 	console.log('server is listining on PORT 3000')
 });
